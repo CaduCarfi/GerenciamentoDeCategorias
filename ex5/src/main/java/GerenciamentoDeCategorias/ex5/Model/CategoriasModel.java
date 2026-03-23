03284d6238840e18d7dfbee0e479eaf26bc45628
CaduCarfi <157553755+CaduCarfi@users.noreply.github.com>
2026-03-23 15:47:53 -0300

    Criação CategoriaModel

---
+++ ex5/src/main/java/GerenciamentoDeCategorias/ex5/Model/CategoriasModel.java
@@ -0,0 +1,49 @@
+package GerenciamentoDeCategorias.ex5.Model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Categoria")
+@Table(name = "tb_categoria")
+public class CategoriasModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nomeCategoria")
+    private String nome;
+    @Column(name = "descricaoCategoria")
+    private String descricao;
+
+    public CategoriasModel() {
+    }
+
+    public CategoriasModel(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
